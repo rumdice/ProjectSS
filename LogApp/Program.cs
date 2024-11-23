7e8351096a7c3732f111aea0fc7d58441f28aaeb
rumdice <rumdice@gmail.com>
2024-11-23 10:22:55 +0900

    신규 프로젝트 LogApp 에 radzen 컴포넌트 적용.

--- LogApp/Program.cs
+++ LogApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using LogApp.Data;
+using Radzen;;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
