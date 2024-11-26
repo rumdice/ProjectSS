a1e6c8abef7a167b5fa497b2fcee11368f7bf3ff
rumdice <rumdice@gmail.com>
2024-11-27 07:32:10 +0900

    MainLayout Blazor에서 Radzen으로 교체, MainLayout 구조 정리 (.cs 코드로 분리)

---
+++ LogApp/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿
+public class MenuItem
+{
+    public string Text { get; set; }
+    public string Path { get; set; }
+    public string Icon { get; set; }
+}
+
+public static class SideMenu
+{
+    public static List<MenuItem> menuItems = new List<MenuItem>
+    {
+        new MenuItem { Text = "Home", Path = "/", Icon = "home" },
+        new MenuItem { Text = "Gallery", Path = "/gallery", Icon = "photo" },
+        new MenuItem { Text = "Image", Path = "/image", Icon = "photo" },
+        new MenuItem { Text = "Item", Path = "/item", Icon = "photo" },
+        new MenuItem { Text = "Temp", Path = "/temp", Icon = "photo" },
+        new MenuItem { Text = "Test", Path = "/test", Icon = "photo" },
+        new MenuItem { Text = "About", Path = "/about", Icon = "info" }
+    };
+}
+
