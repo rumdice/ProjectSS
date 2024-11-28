619f9469e9d7b116a05e115b66424fb14f2be027
rumdice <rumdice@gmail.com>
2024-11-29 07:05:16 +0900

    s3에서 이미지 목록을 받아서 표현

--- LogApp/Service/ImageService.cs
+++ LogApp/Service/ImageService.cs
@@ -10,7 +10,8 @@ using CoreLibrary.Service;
 using CoreLibrary.Database;
 using CoreLibrary.Repository;
 using Microsoft.Extensions.Configuration;
-
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
 //inject IHttpClientFactory HttpClientFactory
 
 // Service : 비즈니스 로직을 처리하는 단계. 간단히 자료를 가져오는 것 부터 복잡한 쿼리 연계까지
@@ -42,103 +43,70 @@ public class ImageService : BaseService
         _logger = logger;
         _s3Client = s3Client;
         _configuration = configuration;
-
     }
 
-    public async Task<List<ItemSimpleEntity>> GetItemSimpleInfoListByNameAsync(string name)
+    ///
+    /// AWS Scrent Manager 에서 정해진 값을 가져온다.
+    ///
+    private static async Task<BasicAWSCredentials> GetSecret()
     {
-        return await _itemRepository.GetItemSimpleInfoByNameList(name);
-    }
+        string secretName = "rumdice-aws-acesss-key";
+        string region = "ap-northeast-2";
 
-    public async Task<ItemSimpleEntity?> GetItemSimpleResultByName(string name)
-    {
-        return await _itemRepository.GetItemSimpleEntityByName(name);
-    }
+        IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
 
-    public async Task LoadImages()
-    {
-        IsLoading = true;
-        ErrorMessage = null;
-        ImageUrls.Clear();
+        GetSecretValueRequest request = new GetSecretValueRequest
+        {
+            SecretId = secretName,
+            VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+        };
+
+        GetSecretValueResponse response;
 
         try
         {
-            var imageUrl = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/bike.jpeg";
-            var imageUrl2 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/light.jpeg";
-            var imageUrl3 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/river.jpeg";
-            var imageUrl4 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/tower.jpeg";
-            var imageUrl5 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/cake.jpeg";
-            var imageUrl6 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/river2.jpeg";
-            var imageUrl7 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/latke1.jpeg";
-            var imageUrl8 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/lake2.jpeg";
-            var imageUrl9 = "https://rumdice-project-ss.s3.ap-northeast-1.amazonaws.com/images/me.jpeg";
-
-
-            ImageUrls.Add(imageUrl);
-            ImageUrls.Add(imageUrl2);
-            ImageUrls.Add(imageUrl3);
-            ImageUrls.Add(imageUrl4);
-            ImageUrls.Add(imageUrl5);
-            ImageUrls.Add(imageUrl6);
-            ImageUrls.Add(imageUrl7);
-            ImageUrls.Add(imageUrl8);
-            ImageUrls.Add(imageUrl9);
-
-
-
-            //이미지를 보내주는 api 서버 통신인데 일단 s3으로 대체 
-            // var client = HttpClientFactory.CreateClient();
-            // var response = await client.GetAsync(imageUrl);
-
-            // if (response.IsSuccessStatusCode)
-            // {
-            //     var images = await response.Content.ReadFromJsonAsync<List<string>>();
-            //     if (images != null)
-            //     {
-            //         imageUrls.AddRange(images);
-            //     }
-            // }
-            // else
-            // {
-            //     errorMessage = $"Failed to load images: {response.StatusCode} - {response.ReasonPhrase}";
-            // }
-
+            response = await client.GetSecretValueAsync(request);
         }
         catch (Exception ex)
         {
-            ErrorMessage = $"An error occurred: {ex.Message}";
+            throw ex;
         }
-        finally
+
+        // JSON 파싱
+        var secret = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(response.SecretString);
+
+        if (secret == null || 
+            !secret.ContainsKey("aws_access_key_id") || 
+            !secret.ContainsKey("aws_secret_access_key"))
         {
-            IsLoading = false;
+            throw new Exception("Secrets JSON is missing required keys.");
         }
+
+        // AWS Credentials 생성
+        return new BasicAWSCredentials(secret["aws_access_key_id"], secret["aws_secret_access_key"]);
     }
 
 
     public async Task LoadImagesByS3()
     {
         IsLoading = true;
-        ErrorMessage = null;
+        ErrorMessage = "";
         ImageUrls.Clear();
 
-        var credentials = new BasicAWSCredentials(
-            _configuration["AWS:AccessKey"], 
-            _configuration["AWS:SecretKey"]);
-
+        var credentials = await ImageService.GetSecret();    
         var clientConfig = new AmazonS3Config
         {
-            RegionEndpoint = RegionEndpoint.GetBySystemName(_configuration["AWS:Region"])
+            RegionEndpoint = RegionEndpoint.GetBySystemName("ap-northeast-2")
         };
 
         _s3Client = new AmazonS3Client(credentials, clientConfig);
 
         try
         {
-            var bucketName = "rumdice-projectss";  // S3 버킷 이름
             var request = new ListObjectsV2Request
             {
-                BucketName = bucketName, // S3 버킷 이름
-                Prefix = "images/"              // 원하는 디렉터리 경로(필요하지 않다면 제거 가능)
+                BucketName = "rumdice-projectss",   // S3 버킷 이름
+                Prefix = "images/"                  // 원하는 디렉터리 경로(필요하지 않다면 제거 가능)
             };
 
             // S3에서 객체 목록을 가져옵니다.
@@ -148,6 +116,11 @@ public class ImageService : BaseService
             {
                 // S3 객체의 URL 생성
                 var imageUrl = $"https://{request.BucketName}.s3.{_region.SystemName}.amazonaws.com/{s3Object.Key}";
+                
+                var imageName = s3Object.Key.Split("/");
+                if (string.IsNullOrEmpty(imageName[1]))
+                    continue;
+                
                 ImageUrls.Add(imageUrl);
             }
         }
