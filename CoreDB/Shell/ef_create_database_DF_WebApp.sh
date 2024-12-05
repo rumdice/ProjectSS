45bfb844b0e2a585ffb280627e6d3f63a1ad25d1
rumdice <rumdice@gmail.com>
2024-12-06 08:31:20 +0900

    불필요한 쉘 스크립트 삭제, dll 업데이트

--- CoreDB/Shell/ef_create_database_DF_WebApp.sh
+++
@@ -1,7 +0,0 @@
-cd ../
-pwd
-rm -rf Database/
-
-dotnet ef dbcontext scaffold -f "Server=localhost;Port=3306;Database=db_WebApp;User=root;Password=pass1234" Pomelo.EntityFrameworkCore.MySql -o DBWebApp
-
-dotnet build
