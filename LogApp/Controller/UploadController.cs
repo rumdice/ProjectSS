295dc235797cb4e83462da0f3c0a33869a2add87
rumdice <rumdice@gmail.com>
2024-12-07 07:14:04 +0900

    이미지를 한번에 여러장 올릴 수 있도록 수정

--- LogApp/Controller/UploadController.cs
+++ LogApp/Controller/UploadController.cs
@@ -21,7 +21,6 @@ public class UploadController : ControllerBase
     [HttpPost("single")]
     public async Task<IActionResult> UploadSingle(IFormFile file)
     {
-
         if (file == null || file.Length == 0)
         {
             return BadRequest("No file received.");
@@ -30,5 +29,41 @@ public class UploadController : ControllerBase
         // S3에 업로드
         await _imageService.UploadFileAsync(file);
         return Ok();
+    }
+
+    [HttpPost("multiple")]
+    public async Task<IActionResult> UploadMultiple([FromForm] List<IFormFile> files)
+    {
+        if (files == null || !files.Any())
+        {
+            return BadRequest("No files received.");
+        }
+
+        var uploadResults = new List<string>();
+
+        foreach (var file in files)
+        {
+            if (file.Length > 0)
+            {
+                try
+                {
+                    // S3에 업로드
+                    var result = await _imageService.UploadFileAsync(file);
+                    uploadResults.Add(result);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Failed to upload file: {file.FileName}");
+                    return StatusCode(500, $"Failed to upload file: {file.FileName}");
+                }
+            }
+            else
+            {
+                _logger.LogWarning($"Empty file received: {file.FileName}");
+            }
+        }
+
+        return Ok(new { UploadedFiles = uploadResults });
+
     }
 }
