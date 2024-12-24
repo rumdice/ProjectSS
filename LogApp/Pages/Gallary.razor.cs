aa66d122b74f9f0fd4c08ef893ebea3ddac94eb3
rumdice <rumdice@gmail.com>
2024-12-25 08:15:53 +0900

    팝업창에 원본 출력 및 이미지 다운로드 기능 추가.

--- LogApp/Pages/Gallary.razor.cs
+++ LogApp/Pages/Gallary.razor.cs
@@ -1,3 +1,4 @@
+using LogApp.Component;
 using LogApp.Service;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
@@ -42,20 +43,4 @@ namespace LogApp.Pages
         }
     }
 
-    public class DialogContent : ComponentBase
-    {
-        [Parameter]
-        public string ImageUrl { get; set; }
-
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
-        {
-            builder.OpenElement(0, "div");
-            builder.AddAttribute(1, "style", "text-align: center; padding: 20px;");
-            builder.OpenElement(2, "img");
-            builder.AddAttribute(3, "src", ImageUrl);
-            builder.AddAttribute(4, "style", "max-width: 100%; max-height: 100%;");
-            builder.CloseElement();
-            builder.CloseElement();
-        }
-    }
 }
