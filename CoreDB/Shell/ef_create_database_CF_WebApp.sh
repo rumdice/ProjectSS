650632dc3ee319a034741aeb95c54d6eaf565632
rumdice <rumdice@gmail.com>
2025-08-01 19:51:09 +0900

    로컬 데이터베이스 초기화로 code frist 방식으로 db 생성 및 스크립트 수정, 대상 로컬 DB 수정

--- CoreDB/Shell/ef_create_database_CF_WebApp.sh
+++ CoreDB/Shell/ef_create_database_CF_WebApp.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+
 cd ../
 pwd
 
+# 기존 마이그레이션 폴더 삭제
 rm -rf Migrations/
 
-dotnet ef database drop --force
+# 데이터베이스 삭제 (명확하게 context 지정)
+dotnet ef database drop --force --context CoreDB.DBWebApp.DbWebAppContext
 
-dotnet ef migrations add InitialCreate
-dotnet ef database update
+# 새 마이그레이션 생성
+dotnet ef migrations add InitialCreate --context CoreDB.DBWebApp.DbWebAppContext 
 
+# (선택) 마이그레이션 적용 단계 - 현재 주석처리된 상태 유지
+dotnet ef database update --context CoreDB.DBWebApp.DbWebAppContext
+
+# 프로젝트 빌드
 dotnet build
-
-
-
--- CoreDB/Shell/ef_create_database_DF_LogApp.sh
+++ CoreDB/Shell/ef_create_database_DF_LogApp.sh
@@ -2,6 +2,6 @@ cd ../
 pwd
 rm -rf Database/
 
-dotnet ef dbcontext scaffold -f "Server=localhost;Port=3306;Database=db_LogApp;User=root;Password=pass1234" Pomelo.EntityFrameworkCore.MySql -o DBLogApp
+dotnet ef dbcontext scaffold -f "Server=localhost;Port=13306;Database=db_LogApp;User=root;Password=pass" Pomelo.EntityFrameworkCore.MySql -o DBLogApp
 
 dotnet build
